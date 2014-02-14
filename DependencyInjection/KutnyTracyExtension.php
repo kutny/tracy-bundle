5339b90e79524598bdc9dad4d6638f291f614f2a
Miha Vrhovnik <miha.vrhovnik@pagein.si>
2014-02-14 12:00:22 +0100

    Configure exceptions directory

--- DependencyInjection/KutnyTracyExtension.php
+++ DependencyInjection/KutnyTracyExtension.php
@@ -5,6 +5,7 @@ namespace Kutny\TracyBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class KutnyTracyExtension extends Extension
@@ -12,8 +13,23 @@ class KutnyTracyExtension extends Extension
 
     public function load(array $configs, ContainerBuilder $container)
     {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $dir = $config['exceptions_directory'];
+        if (null === $dir) {
+            $dir = $container->getParameter('kernel.logs_dir') . '/exceptions';
+        }
+        $container->setParameter('kutny_tracy.exceptions_directory', $dir);
+
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
+
+        if (false !== $dir) {
+            $fs = new Filesystem();
+
+            $fs->mkdir($dir);
+        }
     }
 
 }
